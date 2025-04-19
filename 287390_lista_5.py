4b07f3fcc7a7552d84ffdcf32b0eb0b64060dc1c
brozuss <123550952+brozuss@users.noreply.github.com>
2025-04-19 23:00:49 +0200

    l5_z3

--- 287390_lista_5.py
+++ 287390_lista_5.py
@@ -1,3 +1,7 @@
+from itertools import permutations
+from operator import index
+
+
 def zadanie_1():
     def koduj(napis, klucz=1):
         zakodowane = []
@@ -111,13 +115,102 @@ def zadanie_2():
 
     napis = ['lista']
     lista = ['ala', 'ma', 'kota', 'olek', 'ma', 'kotki']
-    # print(levenshtein('ala ma kota', 'olek ma kotki'))
+
+    print(levenshtein('ala ma kota', 'olek ma kotki'))
     print(guess(napis, lista))
 
 
+def zadanie_3():
+    def brut_force(slownik, wiadomosc):
+        '''
+        wszystkie rozwiazania ale praktycznie nie wykonywalne przez ilość obliczeń: O(n!*m*n)
+        '''
+        for slownik_perm in permutations(slownik):
+            temp_wiad = list(wiadomosc)
+            slownik_elem = {x:list(x) for x in slownik_perm}
+            max_len = len(max(slownik, key= len))
+
+            for i, w in enumerate(temp_wiad):
+                for s in slownik_elem.keys():
+                    if len(slownik_elem[s]) > len(temp_wiad) - i:
+                        break
+                    if w == slownik_elem[s][0]:
+                        slownik_elem[s].remove(w)
+                        if len(slownik_elem[s]) == 0:
+                            slownik_elem[s] = [True]
+                        break
+            solutions = [x for x in slownik_elem.keys() if slownik_elem[x][0] == True]
+            print(solutions)
+
+    def backtracking(message, slownik):
+        """
+        rozwiązanie heurystyczne - nie do końca optymalne rozwiazania, ale poprawne i w rozsądnym czasie - O(2^n)
+        """
+        slownik = sorted(slownik, key=lambda x: -len(x))  # sortowanie malejące
+        best = []
+
+        def func(current, remaining_msg, used_words, start_idx):
+            nonlocal best
+
+            if len(current) > len(best):
+                best = current.copy()
+
+            if len(best) == len(slownik):  # wszysktie słowa
+                return
+
+            for i in range(start_idx, len(slownik)):
+                word = slownik[i]
+                if word in used_words:
+                    continue
+
+                temp_msg = remaining_msg
+                temp_msg_perm = remaining_msg
+                positions = []
+                valid = True
+                for char in word:
+                    pos = temp_msg.find(char)
+                    if pos == -1:
+                        valid = False
+                        break
+
+                    if positions: positions.append(pos + positions[-1] + 1)
+                    else: positions.append(pos)
+
+                    temp_msg = temp_msg[pos + 1:]
+
+                if valid:
+                    used_words.add(word)
+                    current.append(word)
+                    temp_msg_perm = list(temp_msg_perm)
+                    for j in sorted(positions, reverse=True):
+                        del temp_msg_perm[j]
+                    temp_msg_perm = ''.join(temp_msg_perm)
+
+                    func(current, temp_msg_perm, used_words, i + 1)
+                    current.pop()
+                    used_words.remove(word)
+                else:
+                    temp_msg = remaining_msg
+
+        func([], message, set(), 0)
+        return best
+
+
+    slownik = {'kalafior', 'rower', 'krowa', 'pieczarka',
+               'prezydent', 'usa', 'pi', 'sigma', 'python',
+               'naleśniki'
+               }
+
+    wiadomosc = "uslppiapniepyrtswczehazdoyrkcnadvientjqlkjeogijpzxczx"
+
+    # print(brut_force(slownik, wiadomosc))
+    print(backtracking(wiadomosc, slownik))
+
+
 if __name__ == '__main__':
-    # print("zadanie 1:")
-    # zadanie_1()
-    # print("\nzadanie 2:")
+    print("zadanie 1:")
+    zadanie_1()
+    print("\nzadanie 2:")
     zadanie_2()
-    # print("\nzadanie 3:")
+    print("\nzadanie 3:")
+    zadanie_3()
